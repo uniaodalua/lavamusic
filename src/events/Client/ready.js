8a20fbfb5025b07f5f83bcd6db9cb79a929dd23c
DevMoonTv <75825236+DevMoonTv@users.noreply.github.com>
2022-01-05 00:55:45 -0300

    Update ready.js

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -6,10 +6,10 @@ module.exports = async (client) => {
     client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
 
     //Game
-    let statuses = ['/help', `Prefix ${prefix}`];
+    let statuses = ['.lhelp', `Prefix ${prefix}`];
     setInterval(function() {
   		let status = statuses[Math.floor(Math.random()*statuses.length)];
-  		client.user.setActivity(status, {type: "PLAYING"});
+  		client.user.setActivity(status, {type: "LISTENING"});
   	}, 10000)
 
 }
