5efefea3ad13204baa5c04dbfd426a8e8428cab7
DevMoonTv <75825236+DevMoonTv@users.noreply.github.com>
2022-01-05 01:07:52 -0300

    Update about.js

--- src/commands/Information/about.js
+++ src/commands/Information/about.js
@@ -2,9 +2,9 @@ const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
 module.exports = {
     name: "about",
-    category: "Information",
+    category: "informação",
     aliases: [ "botinfo" ],
-    description: "See description about this project",
+    description: "Veja a descrição deste projeto.",
     args: false,
     usage: "",
     permission: [],
@@ -18,24 +18,18 @@ module.exports = {
     .setStyle("LINK")
     .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot`),
     new MessageButton()
-    .setLabel("GitHub")
+    .setLabel("suporte")
     .setStyle("LINK")
-    .setURL("https://github.com/brblacky/lavamusic"),
-    new MessageButton()
-    .setLabel("Support")
-    .setStyle("LINK")
-    .setURL("https://discord.gg/gfcv94hDhv")
+    .setURL("https://discord.io/LuxClub")
 			);
 
       const mainPage = new MessageEmbed()
-            .setAuthor({ name: 'LavaMusic', iconURL: 'https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png'})
-            .setThumbnail('https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png')
+            .setAuthor({ name: 'LuxClub', iconURL: 'https://media.discordapp.net/attachments/900971405477302292/902000220353749062/LOGO_AZUL.png'})
+            .setThumbnail('https://media.discordapp.net/attachments/900971405477302292/902000220353749062/LOGO_AZUL.png')
             .setColor('#303236')
-            .addField('Creator', '[Blacky#6618](https://github.com/brblacky) And [Venom#9718](https://github.com/Venom9718/)', true)
-            .addField('Organization', '[Blacky](https://github.com/brblacky)', true)
-            .addField('Repository', '[Here](https://github.com/brblacky/lavamusic)', true)
+            .addField('Creator', '[DaLua#7582](https://github.com/uniaodalua)', true)
             .addField('\u200b',
-                `[LavaMusic](https://github.com/brblacky/lavamusic/) is [Blacky](https://github.com/brblacky) and [Venom](https://github.com/Venom9718)'s Was created by blacky and Venom. He really wants to make his first open source project ever. Because he wants more for coding experience. In this project, he was challenged to make project with less bugs. Hope you enjoy using LavaMusic!`
+                `◢ Lux Club ◤ é uma casa de festa com uma decoração moderna que cativa a todos. Agitada DJ’s de verdade que tocam ao vivo, 100% em tempo real e sincronizado, os gêneros variam de eletrônica a funk, agradando pessoas com diversos gostos musicais. Localizada em San Fierro.`
             )
         return message.reply({embeds: [mainPage], components: [row]});
     }
