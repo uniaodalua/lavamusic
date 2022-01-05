403d2aaf4d87c0f23c87f92141091c0e0351b0fc
DevMoonTv <75825236+DevMoonTv@users.noreply.github.com>
2022-01-05 00:52:15 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     token: process.env.TOKEN || "",  // your bot token
-    prefix: process.env.PREFIX || "!", // bot prefix
-    ownerID: process.env.OWNERID || "491577179495333903", //your discord id
+    prefix: process.env.PREFIX || "l.", // bot prefix
+    ownerID: process.env.OWNERID || "441367221105852416", //your discord id
     SpotifyID: process.env.SPOTIFYID || "", // spotify client id
     SpotifySecret: process.env.SPOTIFYSECRET || "", // spotify client secret
     mongourl: process.env.MONGO_URI || "", // MongoDb URL
-    embedColor: process.env.COlOR || "#303236", // embed colour
-    logs: process.env.LOGS || "875254787191504947", // channel id for guild create and delete logs 
+    embedColor: process.env.COlOR || "#0045f7", // embed colour
+    logs: process.env.LOGS || "928131404221526048", // channel id for guild create and delete logs 
 
     nodes: {
 
